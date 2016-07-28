2818dedf953dea65ef32430eb082855adf7978ff
tobby <tobby@tobby.eu>
2016-07-28 02:42:09 +0200

    Add funtion to dynamically subscribe (test) topic like prescribed by the API

--- src/testoutput.cpp
+++ src/testoutput.cpp
@@ -26,21 +26,28 @@ bool TestOutput::Connect()
   header.seq++;
   hello.request.header = header;
   hello.request.name = "OutputTest";
-  uuid_t uuid;
+  /*uuid_t uuid;
   uuid_generate_random (uuid);
   char uuid_string[37];
-  uuid_unparse (uuid, uuid_string);
-  //hello.request.uuid = uuid_string;
-  hello.request.uuid = "TestOutputClientUUID";
+  uuid_unparse (uuid, uuid_string);*/
+
+  string uuid_string;
+  uuid_string = "TestOutputClientUUID";
+
+  this->uuid = uuid_string;
+  hello.request.uuid = uuid_string;
   DeviceType deviceType = DeviceType::OutputDevice;
   hello.request.type = (unsigned short) deviceType;
   tobbytestoutput::Feature feature1;
   feature1.type = (unsigned short) FeatureType::Switch;
   feature1.name = "Button Test";
-  uuid_generate_random (uuid);
-  uuid_unparse (uuid, uuid_string);
-  //feature1.uuid = uuid_string;
-  feature1.uuid = "TestOutputFeatureUUID";
+  /*uuid_generate_random (uuid);
+  uuid_unparse (uuid, uuid_string);*/
+
+  uuid_string = "TestOutputFeatureUUID";
+
+  featureUuid = uuid_string;
+  feature1.uuid = uuid_string;
   vector<tobbytestoutput::Feature> features;
   features.push_back(feature1);
   hello.request.features = features;
@@ -61,4 +68,14 @@ bool TestOutput::Connect()
 void TestOutput::readConfigMsg(const tobbytestoutput::Config::ConstPtr& msg)
 {
   ROS_INFO("Config Msg for: %s, should be connected to publisher %s with feature id %s to feature id %s, coefficient: %3.2f", msg->receiverUUID.c_str(), msg->publisherUUID.c_str(), msg->publisherFeatureUUID.c_str(), msg->receiverFeatureUUID.c_str(), msg->coefficient);
+  if (msg->receiverUUID == uuid && msg->receiverFeatureUUID == featureUuid)
+  {
+    ROS_INFO("This message is for me! :)");
+    featureSub = nh->subscribe("TobbyAPI/" + msg->publisherUUID + "/" + msg->publisherFeatureUUID, 1000, &TestOutput::gotData, this);
+  }
+}
+
+void TestOutput::gotData(const std_msgs::Bool::ConstPtr& msg)
+{
+  ROS_INFO("Status: %d", msg->data);
 }
--- src/testoutput.hpp
+++ src/testoutput.hpp
@@ -5,6 +5,7 @@
 #include "tobbytestoutput/Hello.h"
 #include "ros/ros.h"
 #include "std_msgs/Header.h"
+#include "std_msgs/Bool.h"
 #include <unordered_map>
 
 using namespace ros;
@@ -15,10 +16,13 @@ class TestOutput
 private:
   NodeHandle *nh;
   ServiceClient helloClient;
-  Subscriber configSub;
+  Subscriber configSub, featureSub;
   std_msgs::Header header;
   bool connected;
+  string uuid;
+  string featureUuid;
   void readConfigMsg(const tobbytestoutput::Config::ConstPtr& msg);
+  void gotData(const std_msgs::Bool::ConstPtr& msg);
 
 public:
   TestOutput(NodeHandle *nh);
