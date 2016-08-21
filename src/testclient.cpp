835a729ea29043f378894de1d0cd9e2e527f0e7a
tobby <tobby@tobby.eu>
2016-08-21 05:07:12 +0200

    Replace '-' by '_' in UUIDs because ROS doesn't allow these as topic names

--- src/testclient.cpp
+++ src/testclient.cpp
@@ -2,6 +2,7 @@
 #include "testclient.hpp"
 #include "tobbytestclient/Feature.h"
 #include "tobbytestclient/Hello.h"
+#include <algorithm>
 #include <uuid/uuid.h>
 
 using namespace ros;
@@ -26,8 +27,10 @@ bool TestClient::Connect()
   hello.request.name = "leckomio";
   uuid_t uuid;
   uuid_generate_random (uuid);
-  char uuid_string[37];
-  uuid_unparse (uuid, uuid_string);
+  char uuid_array[37];
+  uuid_unparse(uuid, uuid_array);
+  string uuid_string(uuid_array);
+  replace(uuid_string.begin(), uuid_string.end(), '-', '_');
   hello.request.uuid = uuid_string;
   Device_Type deviceType = Device_Type::Input_Device;
   hello.request.type = (unsigned short) deviceType;
@@ -35,13 +38,17 @@ bool TestClient::Connect()
   feature1.type = (unsigned short) Feature_Type::Camera;
   feature1.name = "Kamera vorne (hinten oder so)";
   uuid_generate_random (uuid);
-  uuid_unparse (uuid, uuid_string);
+  uuid_unparse(uuid, uuid_array);
+  uuid_string = uuid_array;
+  replace(uuid_string.begin(), uuid_string.end(), '-', '_');
   feature1.uuid = uuid_string;
   tobbytestclient::Feature feature2;
   feature2.type = (unsigned short) Feature_Type::Camera;
   feature2.name = "Kamera hinten (wirklich hinten!)";
   uuid_generate_random (uuid);
-  uuid_unparse (uuid, uuid_string);
+  uuid_unparse(uuid, uuid_array);
+  uuid_string = uuid_array;
+  replace(uuid_string.begin(), uuid_string.end(), '-', '_');
   feature2.uuid = uuid_string;
   vector<tobbytestclient::Feature> features;
   features.push_back(feature1);
