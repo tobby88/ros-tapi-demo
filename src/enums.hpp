3c47392787fa824c37699fc9acbdda6b3402808a
tobby <tobby@tobby.eu>
2016-08-22 05:57:16 +0200

    Merge API changes

--- src/enums.hpp
+++ src/enums.hpp
@@ -1,8 +1,24 @@
 #ifndef ENUMS_H
 #define ENUMS_H
 
-enum class Device_Type {Input_Device, Output_Device, Hook};
-enum class Device_Status_Response {OK, Error};
-enum class Feature_Type {Camera, Button, Axis};
+enum class DeviceStatusResponse
+{
+  OK,
+  Error
+};
+
+enum class DeviceType
+{
+  SenderDevice,
+  ReceiverDevice,
+};
+
+enum class FeatureType
+{
+  Images,
+  Switch,
+  Tristate,
+  Axis
+};
 
 #endif // ENUMS_H
--- src/testclient.cpp
+++ src/testclient.cpp
@@ -21,8 +21,8 @@ bool TestClient::Connect()
   tobbytestclient::Hello hello;
   header.stamp = Time::now();
   header.seq++;
-  hello.request.header = header;
-  hello.request.name = "leckomio";
+  hello.request.Header = header;
+  hello.request.Name = "leckomio";
 
   uuid_t uuid;
   uuid_generate_random(uuid);
@@ -30,47 +30,47 @@ bool TestClient::Connect()
   uuid_unparse(uuid, uuid_array);
   string uuid_string(uuid_array);
   replace(uuid_string.begin(), uuid_string.end(), '-', '_');
-  hello.request.uuid = uuid_string;
+  hello.request.UUID = uuid_string;
 
-  Device_Type deviceType = Device_Type::Input_Device;
-  hello.request.type = (unsigned short)deviceType;
+  DeviceType deviceType = DeviceType::SenderDevice;
+  hello.request.DeviceType = (unsigned short)deviceType;
 
   tobbytestclient::Feature feature1;
-  feature1.type = (unsigned short)Feature_Type::Camera;
-  feature1.name = "Kamera vorne (hinten oder so)";
+  feature1.FeatureType = (unsigned short)FeatureType::Images;
+  feature1.Name = "Kamera vorne (hinten oder so)";
   uuid_generate_random(uuid);
   uuid_unparse(uuid, uuid_array);
   uuid_string = uuid_array;
   replace(uuid_string.begin(), uuid_string.end(), '-', '_');
-  feature1.uuid = uuid_string;
+  feature1.UUID = uuid_string;
 
   tobbytestclient::Feature feature2;
-  feature2.type = (unsigned short)Feature_Type::Camera;
-  feature2.name = "Kamera hinten (wirklich hinten!)";
+  feature2.FeatureType = (unsigned short)FeatureType::Images;
+  feature2.Name = "Kamera hinten (wirklich hinten!)";
   uuid_generate_random(uuid);
   uuid_unparse(uuid, uuid_array);
   uuid_string = uuid_array;
   replace(uuid_string.begin(), uuid_string.end(), '-', '_');
-  feature2.uuid = uuid_string;
+  feature2.UUID = uuid_string;
 
   tobbytestclient::Feature feature3;
-  feature3.type = (unsigned short)Feature_Type::Button;
-  feature3.name = "irgendson Button";
+  feature3.FeatureType = (unsigned short)FeatureType::Switch;
+  feature3.Name = "irgendson Button";
   uuid_generate_random(uuid);
   uuid_unparse(uuid, uuid_array);
   uuid_string = uuid_array;
   replace(uuid_string.begin(), uuid_string.end(), '-', '_');
-  feature3.uuid = uuid_string;
+  feature3.UUID = uuid_string;
 
   vector<tobbytestclient::Feature> features;
   features.push_back(feature1);
   features.push_back(feature2);
   features.push_back(feature3);
-  hello.request.features = features;
+  hello.request.Features = features;
   if (helloClient.call(hello))
   {
     ROS_INFO("Connection established, Status %u, Heartbeat %u",
-             hello.response.status, hello.response.heartbeat);
+             hello.response.Status, hello.response.Heartbeat);
   }
   else
   {
