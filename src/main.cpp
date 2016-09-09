f1ee363076f37819e9d4a60cb16260320f012d34
tobby <tobby@tobby.eu>
2016-09-10 01:05:48 +0200

    Add copyright

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 /*******************************************************************************
+*  Copyright (C) 2016 by Tobias Holst                                          *
+*                                                                              *
 *  This file is part of tapi_testsubscriber.                                   *
 *                                                                              *
 *  tapi_testsubscriber is free software: you can redistribute it and/or modify *
