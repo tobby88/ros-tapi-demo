2c8536fcb64c367fe3638fe14745b63a57bb5414
tobby <tobby@tobby.eu>
2016-09-10 01:04:54 +0200

    Add copyright

--- src/testpublisher.cpp
+++ src/testpublisher.cpp
@@ -1,4 +1,6 @@
 /******************************************************************************
+*  Copyright (C) 2016 by Tobias Holst                                         *
+*                                                                             *
 *  This file is part of tapi_testpublisher.                                   *
 *                                                                             *
 *  tapi_testpublisher is free software: you can redistribute it and/or modify *
