b789b54670e1ed36e6ebfe1d7896516986429da3
tobby <tobby@tobby.eu>
2016-09-21 18:43:05 +0200

    Move files to their new place

--- src/testpublisher.cpp
+++ src/testpublisher.cpp
@@ -73,3 +73,20 @@ void TestPublisher::SendTest()
   number += 1.0001;
 }
 }
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "Tapi_TestPublisher");
+  ros::NodeHandle nh;
+  Tapi::TestPublisher testPublisher(&nh);
+  ros::Rate loop_rate(1);
+  while (ros::ok())
+  {
+    ros::spinOnce();
+    loop_rate.sleep();
+    testPublisher.SendTest();
+    ros::spinOnce();
+  }
+
+  return 0;
+}
--- src/testsubscriber.cpp
+++ src/testsubscriber.cpp
@@ -1,29 +1,29 @@
 /*******************************************************************************
  *  Copyright (C) 2016 by Tobias Holst                                         *
  *                                                                             *
- *  This file is part of tapi_testpublisher.                                   *
+ *  This file is part of tapi_testsubscriber.                                  *
  *                                                                             *
- *  tapi_testpublisher is free software: you can redistribute it and/or modify *
+ *  tapi_testsubscriber is free software: you can redistribute it and/or modify*
  *  it under the terms of the GNU General Public License as published by       *
  *  the Free Software Foundation, either version 3 of the License, or          *
  *  (at your option) any later version.                                        *
  *                                                                             *
- *  tapi_testpublisher is distributed in the hope that it will be useful,      *
+ *  tapi_testsubscriber is distributed in the hope that it will be useful,     *
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of             *
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              *
  *  GNU General Public License for more details.                               *
  *                                                                             *
  *  You should have received a copy of the GNU General Public License          *
- *  along with tapi_testpublisher.  If not, see <http://www.gnu.org/licenses/>.*
+ *  along with tapi_testsubscriber. If not, see <http://www.gnu.org/licenses/>.*
  *                                                                             *
- *  Diese Datei ist Teil von tapi_testpublisher.                               *
+ *  Diese Datei ist Teil von tapi_testsubscriber.                              *
  *                                                                             *
- *  tapi_testpublisher ist Freie Software: Sie können es unter den Bedingungen *
+ *  tapi_testsubscriber ist Freie Software: Sie können es unter den Bedingungen*
  *  der GNU General Public License, wie von der Free Software Foundation,      *
  *  Version 3 der Lizenz oder (nach Ihrer Wahl) jeder späteren                 *
  *  veröffentlichten Version, weiterverbreiten und/oder modifizieren.          *
  *                                                                             *
- *  tapi_testpublisher wird in der Hoffnung, dass es nützlich sein wird, aber  *
+ *  tapi_testsubscriber wird in der Hoffnung, dass es nützlich sein wird, aber *
  *  OHNE JEDE GEWÄHRLEISTUNG, bereitgestellt; sogar ohne die implizite         *
  *  Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK  *
  *  Siehe die GNU General Public License für weitere Details.                  *
@@ -32,21 +32,52 @@
  *  Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>. *
  ******************************************************************************/
 
-#include "ros/ros.h"
-#include "testpublisher.hpp"
+#include "testsubscriber.hpp"
+#include <uuid/uuid.h>
+
+using namespace std;
+
+namespace Tapi
+{
+TestSubscriber::TestSubscriber(ros::NodeHandle* nh) : nh(nh)
+{
+  tsub = new Tapi::Subscriber(nh, "TestSubscriber");
+  ros::SubscribeOptions opt;
+  opt = SubscribeOptionsForTapi(std_msgs::Bool, 5, &TestSubscriber::gotDataBool);
+  tsub->AddFeature(opt, "Bool Topic");
+  coefficient =
+      tsub->AddFeature(SubscribeOptionsForTapi(std_msgs::Float64, 5, &TestSubscriber::gotDataFloat), "Float Topic");
+}
+
+TestSubscriber::~TestSubscriber()
+{
+  delete tsub;
+}
+
+void TestSubscriber::gotDataBool(const std_msgs::Bool::ConstPtr& msg)
+{
+  if (msg->data)
+    ROS_INFO("Bool: true");
+  else
+    ROS_INFO("Bool: false");
+}
+
+void TestSubscriber::gotDataFloat(const std_msgs::Float64::ConstPtr& msg)
+{
+  double value;
+  value = msg->data * *coefficient;
+  ROS_INFO("Double/Float64: %f with coefficient %f", value, *coefficient);
+}
+}
 
 int main(int argc, char** argv)
 {
-  ros::init(argc, argv, "Tapi_TestPublisher");
+  ros::init(argc, argv, "Tapi_TestSubscriber");
   ros::NodeHandle nh;
-  Tapi::TestPublisher testPublisher(&nh);
-  ros::Rate loop_rate(1);
+  Tapi::TestSubscriber testSubscriber(&nh);
   while (ros::ok())
   {
-    ros::spinOnce();
-    loop_rate.sleep();
-    testPublisher.SendTest();
-    ros::spinOnce();
+    ros::spin();
   }
 
   return 0;
